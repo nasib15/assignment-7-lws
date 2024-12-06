6855bd55e264f7aa8159ce0f5ccca1c376c10226
nasib15 <nasibhossain57@gmail.com>
2024-12-06 19:17:22 +0600

    Add: PATCH api for videos

--- app/api/videos/[id]/route.js
+++ app/api/videos/[id]/route.js
@@ -6,6 +6,45 @@ export async function GET(request, { params }) {
   return NextResponse.json(getVideoById(params.id));
 }
 
+export async function PATCH(request, { params }) {
+  const video = await request.json();
+  const videoId = params.id;
+  const videoIndex = getAllVideos().findIndex(
+    (video) => video.videoId === videoId
+  );
+
+  // Validate request body fields
+  const allowedFields = ["title", "description"];
+  const receivedFields = Object.keys(video);
+  const invalidFields = receivedFields.filter(
+    (field) => !allowedFields.includes(field)
+  );
+
+  if (invalidFields.length > 0) {
+    return NextResponse.json(
+      {
+        status: "ERROR",
+        message:
+          "Invalid fields provided. Only title and description are allowed",
+      },
+      { status: 400 }
+    );
+  }
+
+  const { title, description } = video;
+
+  const updatedVideo = {
+    ...getAllVideos()[videoIndex],
+    ...(title && { title }),
+    ...(description && { description }),
+  };
+
+  return NextResponse.json({
+    status: "OK",
+    updatedVideo,
+  });
+}
+
 export async function DELETE(request, { params }) {
   const videoId = params.id;
   const videoIndex = getAllVideos().findIndex(
